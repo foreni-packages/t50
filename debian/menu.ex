25e03c26e1d83f87ef0daa1376f0fdd4db9f4155
Devon Kearns <dookie@kali.org>
2012-12-26 11:29:47 -0700

    Pre-build commit

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(t50):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="t50" command="/usr/bin/t50"
--- debian/t50.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the t50 package
-#
-0 4	* * *	root	[ -x /usr/bin/t50_maintenance ] && /usr/bin/t50_maintenance
